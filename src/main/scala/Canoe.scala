51519a5fea6d552cf54938788c2772d786e1ff0f
jtgi <j@jtgi.me>
2015-05-03 16:40:16 -0700

    some failing tests

--- src/main/scala/Canoe.scala
+++ src/main/scala/Canoe.scala
@@ -6,29 +6,48 @@ import scala.concurrent.duration._
  * Created by jtgi on 5/2/15.
  */
 object Canoe extends App {
+  // Temp Config
+  val electionTimeoutMin = 10 millis
+  val electionTimeoutMax = 100 millis
+
+  // Init Actors
   val system = ActorSystem("canoe")
   val worker = system.actorOf(Props[CanoeWorker], "canoeworker")
 }
 
 // Messages
-case object AppendEntries
+case class AppendEntries(term: Int, leader: ActorRef, prevLogIndex: Int, prevLogTerm: Int, entries: List[Any])
+case class AppendEntriesRes(term: Int, success: Boolean)
+case class RequestVote(term: Int, candidate: ActorRef, lastLogIndex: Int, lastLogTerm: Int)
+case class RequestVoteRes(term: Int, voteGranted: Boolean)
 
-// FSM
+// States
 sealed trait State
 case object Follower extends State
 case object Candidate extends State
 case object Leader extends State
 
+// Data
 sealed trait Data
 case object Unitialized extends Data
 
 class CanoeWorker extends LoggingFSM[State, Data] {
+  val canoeLog = new InMemoryCanoeLog
 
   startWith(Follower, Unitialized)
 
   when(Follower, stateTimeout = 2 seconds) {
-    case Event(FSM.StateTimeout, Unitialized) =>
-      goto(Candidate) using Unitialized
+    case Event(FSM.StateTimeout, _) =>
+      goto(Candidate)
+
+    case Event(m @ AppendEntries(_, _, _, _, Nil), _) =>
+      log.debug("Heartbeat")
+      stay()
+
+    case Event(m @ AppendEntries, _) => stay()
+    case Event(m @ AppendEntriesRes, _) => stay()
+    case Event(m @ RequestVote, _) => stay()
+    case Event(m @ RequestVoteRes, _) => stay()
   }
 
   when(Candidate, stateTimeout = 2 seconds) {
@@ -43,6 +62,26 @@ class CanoeWorker extends LoggingFSM[State, Data] {
 
   initialize();
 
+}
+
+trait CanoeLog {
+  def append(entry: CanoeLogEntry): Unit
+  def get(index: Int): Option[CanoeLogEntry]
+  def last: Option[CanoeLogEntry]
+}
+
+case class CanoeLogEntry(index: Int, term: Int, msg: String)
+
+class InMemoryCanoeLog extends CanoeLog {
+  val log = List[CanoeLogEntry]()
+
+  def append(entry: CanoeLogEntry) = entry :: log
+
+  def get(index: Int): Option[CanoeLogEntry] = {
+    if(log isDefinedAt index) Some(log(index)) else None
+  }
+
+  def last = log.headOption
 }
 
 
--- src/test/scala/CanoeWorkerSpec.scala
+++ src/test/scala/CanoeWorkerSpec.scala
@@ -20,10 +20,36 @@ class CanoeWorkerSpec(_system: ActorSystem)
     system.awaitTermination(10 seconds)
   }
 
-  "An FSM" should "be initialized" in {
-    val fsm = TestFSMRef(new CanoeWorker);
-    fsm.setState(Follower, Unitialized)
+  "A canoe worker" should "be initialized to the Follower state" in {
+    val fsm = TestFSMRef(new CanoeWorker)
     fsm.stateName should be (Follower)
-    fsm.stateData should be (Unitialized)
   }
+
+  "A follower" should "transition to candidate after timing out" in ???
+  "A follower" should "treat AppendEntries as a heartbeat when there is no entry data" in ???
+  "A follower" should "refuse an AppendEntries command when its log does not contain the (prevLogIndex, prevLogTerm)" in ???
+  "A follower" should "be sent a diff of the log when its log is out of date" in ???
+  "A follower" should "deny its vote when a candidate has a shorter log" in ???
+  "A follower" should "deny its vote when a candidates log has a lower term" in ???
+  "A follower" should "grant its vote when it has not yet voted for the current term and candidates log is at up to date with its log" in ???
+
+  "A candidate" should "broadcast a RequestVote message upon entering candidate state" in ???
+  "A candidate" should "vote for itself when it begins an election" in ???
+  "A candidate" should "transition to a leader when it receives a majority of votes" in ???
+  "A candidate" should "transition to follower if it receives an AppendEntries with a term >= to its own" in ???
+  "A candidate" should "return a failure response when an AppendEntries message is sender's term is < its own" in ???
+
+  "A leader" should "send heartbeats to its followers periodically" in ???
+  "A leader" should "append a command to its log when it receives a client request" in ???
+  "A leader" should "send AppendEntries to each of its followers when it receives a client request" in ???
+  "A leader" should "apply the operation to its data store when it receives a majority vote" in ???
+  "A leader" should "retry AppendEntries decrementing nextIndex when a follower refuses its request" in ???
+  "A leader" should "append all entries up to the its latest index when a follower is behind" in ???
+  "A leader" should "keep track of each of its follower's highest replicated log entry index" in ???
+
+  // Perhaps?
+  "A leader" should "transition to follower if it receives an AppendEntries with a term >= to its own" in ???
+
+  "Two log entries" should "be identical when they have the same index and term" in ???
+  "Two logs" should "be identical up to the same index and term" in ???
 }
